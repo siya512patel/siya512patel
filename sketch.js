d02531268ff59fb278d4d61dfd4d63ce0da7b08c
siya512patel <99736017+siya512patel@users.noreply.github.com>
2022-03-01 16:15:51 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -62,16 +62,6 @@ function draw() {
   if(path.y > 400 ){
     path.y = height/2;
   }
-
-  if(path.y > 400 ){
-    path.y = height/2;
-  }
-
-  if(path.y > 400 ){
-    path.y = height/2;}
-
-  if(path.y > 400 ){
-    path.y = height/2;}
-  
+      
   drawSprites();
 }
